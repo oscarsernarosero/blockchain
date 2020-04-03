01ec427c7537eead8c96f06b369a8ece5b0ade4f
Oscar Serna <oscar.eduardo.serna@gmail.com>
2020-04-02 21:46:03 -0500

    modules for Transaction classes and Accounts created

---
+++ bitcoin/basics/final/.ipynb_checkpoints/transactions-checkpoint.py
@@ -0,0 +1,315 @@
+"""
+This code makes use of the code written by Jimmy Song in the book Programming Bitcoin.
+This code only tries to build on Jimmi Song's code and take it to the next step as 
+suggested by him in chapter 14 of his book.
+This module is coded entirely by Oscar Serna.
+
+This is just an educational-purpose code. The author does not take any responsibility
+on any losses caused by the use of this code.
+"""
+
+from ecc import PrivateKey, S256Point, Signature
+from script import p2pkh_script, p2sh_script, Script, p2wpkh_script,p2wsh_script
+from helper import (
+    decode_base58, SIGHASH_ALL, h160_to_p2pkh_address, hash160, 
+    h160_to_p2sh_address, encode_varint)
+from tx import TxIn, TxOut, Tx, TxFetcher
+import hashlib
+from bip32 import Xtended_privkey, Xtended_pubkey
+from bip39 import Mnemonic
+import segwit_addr
+
+
+
+class Transact:
+    
+    @classmethod
+    def get_index(self,outs_list, address):    
+        """
+        For later: implement looking for multiple indexes in the same tx.
+        """
+       
+        print(f"Address: {address}")
+        if address[:2] in ["tb","bc"]:
+            address = self.decode_p2wpkh_addr(address)
+            for index,out in enumerate(outs_list):
+                if out.script_pubkey.cmds[1] == address or out.script_pubkey.cmds[0] == address:
+                    return index
+        else:
+            address = decode_base58(address)
+            for index,out in enumerate(outs_list):
+                if out.script_pubkey.cmds[2] == address or out.script_pubkey.cmds[1] == address:
+                    return index
+        
+        
+        raise Exception( "output index not found")
+
+    @classmethod
+    def get_amount_utxo(cls,outs_list, index):
+        """
+        Supporting method.
+        receives the list of outputs from transaction and the index of 
+        particular output of interest and returns the amount of the UTXO.
+        outs_list: is the list of outputs of previous transaction where the UTXO is.
+        index: the index of the UTXO in the list of all the outputs.
+        """
+        return outs_list[index].amount
+
+    @classmethod
+    def get_tx_ins_utxo(self,prev_tx_id_list, receiving_address, testnet=True):
+        """
+        Receives a list of transaction ids where the UTXOs to spend are, and 
+        also the receiving address to return a valid tx_in list to create
+        a transaction.
+        prev_tx_id_list: list of the transaction ids where the UTXOs are.
+        receiving_address: the address trying to spend the UTXO (String).
+        testnet: if the transaction is in testnet or not (boolean).
+        """
+        tx_ins = []
+
+        for prev_tx_id in prev_tx_id_list:
+            prev_tx = TxFetcher.fetch(prev_tx_id, testnet)
+            prev_index = self.get_index(prev_tx.tx_outs, receiving_address)
+            tx_in = TxIn(bytes.fromhex(prev_tx_id),prev_index)
+            utxo = self.get_amount_utxo(prev_tx.tx_outs, prev_index)
+            #print(f"index 1: {prev_index}, amount: {utxo}")
+            tx_ins.append({"tx_in": tx_in, "utxo": utxo})
+
+        return tx_ins
+
+    @classmethod
+    def calculate_fee(cls,version, tx_ins, tx_outs, locktime, privkey, redeem_script, 
+                      testnet=True, multisig =False, fee_per_byte = 8, segwit = False ):
+        """
+        privkey: can be just one or a list of private keys in the case of multisignature.
+        """
+        my_tx = Tx(1, tx_ins, tx_outs, 0, testnet=True)
+        print(my_tx)
+
+        # sign the inputs in the transaction object using the private key
+        if multisig:
+            #for tx_input in range(len(tx_ins)):
+             #   print(my_tx.sign_input_multisig(tx_input, privkey, redeem_script))
+                pass
+        else:
+            for tx_input in range(len(tx_ins)):
+                print(my_tx.sign_input(tx_input, privkey, segwit = segwit))
+            # print the transaction's serialization in hex
+
+        #Let's calculate the fee and the change:
+        tx_size = len(my_tx.serialize().hex())
+        #fee_per_byte = 8 # I changed from 2 to 10 after sending this transaction because it had really low appeal to miners.
+        if multisig: tx_size*=2
+        fee = tx_size * fee_per_byte
+        print(f"fee: {fee}")
+        return fee
+    
+    @classmethod
+    def calculate_change(cls, utxo_list, fee, amountTx):
+        """
+        utxo_list: the list of the amounts of every utxo.
+        amountTx: the list of the ammounts of every transaction output.
+        fee: the fee of the transaction.
+        Returns the respective amount of the change.
+        """
+        total_utxo = sum(utxo_list)
+        total_out = sum(amountTx)
+        change = total_utxo - fee - total_out
+        print(f"change {change}")
+        if change < 0:
+            raise Exception( f"Not enough utxos: total_utxo {total_utxo} and total_out {total_out} meaning change = {change}")
+        #Let's make sure that we are actually spending the exact amount of the UTXO
+        total_send=fee+total_out+change
+        diff = total_utxo-total_send
+        print(f"total {total_send}, diff: {diff}")
+
+        return change
+    
+    @classmethod
+    def decode_p2wpkh_addr(self,address):
+        decoded = segwit_addr.bech32_decode(address)
+        str_pubkeyhash = ""
+        for char in decoded[1][1:]:
+            str_pubkeyhash += "{0:05b}".format(char)
+        return int(str_pubkeyhash,2).to_bytes(20,"big")
+    
+class Transaction(Transact):
+      
+    def __init__(self,transaction,sender_account,tx_ins,utxos ,tx_outs, fee, change, testnet, segwit):
+        """
+        utxo_tx_id_list: the list of the transaction ids where the UTXOs are.
+        receivingAddress_w_amount_list: a list of tuples (to_address,amount) specifying
+        the amount to send to each address.
+        sender_account: must be an Account object.
+        If fee is specifyed, then the custom fee will be applied.
+        """
+        self.transaction = transaction
+        self.sender_account = sender_account
+        self.tx_ins = tx_ins
+        self.utxos = utxos
+        self.fee = fee
+        self.segwit = segwit
+        self.tx_outs = tx_outs
+        self.testnet = testnet
+        self.change = change
+        
+    @classmethod
+    def validate_data(self,sender_account_address,outputAddress_amount_list ):
+        if sender_account_address[0] in "2mnt":
+            testnet = True
+            for addr in outputAddress_amount_list:
+                if addr[0][0] not in "2mnt":
+                    raise Exception (f"{addr[0]} not a testnet address. Funds will be lost!")
+                if addr[1] < 1:
+                    raise Exception (f"{addr[1]} not a valid amount. It should be greater than 1 satoshis")
+        else:
+            testnet  = False
+            for addr in outputAddress_amount_list:
+                if addr[0][0] not in "13b":
+                    raise Exception (f"{addr[0]} not a mainnet bitcoin address. Funds will be lost!")
+                if addr[1] < 1:
+                    raise Exception (f"{addr[1]} not a valid amount. It should be greater than 1 satoshis")
+              
+        return testnet
+    
+    @classmethod
+    def get_outputs(self, receivingAddress_w_amount_list, account):
+        
+        #https://en.bitcoin.it/wiki/List_of_address_prefixes
+        #We create the tx outputs based on the kind of address
+        tx_outs = []
+        for output in receivingAddress_w_amount_list:
+            
+            if output[0][0] in "1mn" :
+                tx_outs.append( TxOut(output[1], p2pkh_script(decode_base58(output[0]))))
+            #For multidignature p2sh
+            elif output[0][0] in "23" :
+                tx_outs.append( TxOut(output[1], p2sh_script(decode_base58(output[0]))))
+            elif output[0][:2] in ["bc","tb"] :
+                tx_outs.append( TxOut(output[1], p2wpkh_script(self.decode_p2wpkh_addr(output[0]))))#Needs to be tested
+            else:
+                raise Exception ("Not supported address.")
+        
+        #Let's return the fake change to our same address. 
+        #This will change later when BIP32 is implemented.
+        if account.addr_type == "p2pkh":
+            tx_outs.append( TxOut(1000000, p2pkh_script(decode_base58(account.address))))
+        elif account.addr_type == "p2wpkh":
+            tx_outs.append( TxOut(1000000, p2wpkh_script(self.decode_p2wpkh_addr(account.address))))
+        elif account.addr_type in  ["p2sh","p2sh_p2wpkh"]:
+            tx_outs.append( TxOut(1000000, p2sh_script(decode_base58(account.address))))
+        
+        return tx_outs
+                
+    @classmethod
+    def get_inputs(self,utxo_tx_id_list, sender_address, testnet):
+        return self.get_tx_ins_utxo(utxo_tx_id_list, sender_address, testnet)
+         
+    
+    @classmethod
+    def unsigned_tx(self,tx_ins, tx_outs, testnet, segwit, sender_account, utxos,receivingAddress_w_amount_list):
+        #CREATING THE TRANSACTION RIGHT HERE:
+        my_tx = Tx(1, tx_ins, tx_outs, 0, testnet=testnet, segwit=segwit)#check for segwit later!!!!
+        
+        #CHECK THE FOLLOWING LINE LATER!! 
+        if sender_account.addr_type not in  ["p2sh","p2wsh","p2sh_p2wsh"]:
+            fee = self.calculate_fee(1, tx_ins, tx_outs, 0, privkey=sender_account.privkey, 
+                                         redeem_script=None, testnet=testnet, segwit = segwit)
+            
+            change = self.calculate_change(utxos, fee, [x[1] for x in receivingAddress_w_amount_list])
+
+        else: raise Exception ("This is a multisignature transaction. Use MultiSigTransaction instead of Transaction.")
+        
+        my_tx.tx_outs[-1].amount = change
+        return fee, change, my_tx
+        
+        
+    @classmethod
+    def sign_tx(self, tx_ins, transaction, sender_account, segwit):
+        p2sh = False
+        if sender_account.addr_type in ["p2sh","p2sh_p2wpkh"]: p2sh = True
+        for tx_input in range(len(tx_ins)):
+            if not transaction.sign_input(tx_input, sender_account.privkey, segwit = segwit, p2sh = p2sh):
+                return False
+        return True
+    
+    @classmethod
+    def create(self, utxo_tx_id_list, receivingAddress_w_amount_list, sender_account, fee=None, #Modify code to allow manual fee!!!
+                 segwit=False):
+        """
+        utxo_tx_id_list: the list of the transaction ids where the UTXOs are.
+        receivingAddress_w_amount_list: a list of tuples (to_address,amount) specifying
+        the amount to send to each address.
+        sender_account: must be an Account object.
+        If fee is specifyed, then the custom fee will be applied.
+        """
+        testnet = self.validate_data(sender_account.address,receivingAddress_w_amount_list)
+        tx_outs = self.get_outputs(receivingAddress_w_amount_list, sender_account)
+        tx_ins_utxo = self.get_inputs(utxo_tx_id_list, sender_account.address, testnet)
+        tx_ins = [x["tx_in"] for x in tx_ins_utxo]
+        utxos = [x["utxo"] for x in tx_ins_utxo]
+        fee, change, transaction = self.unsigned_tx(tx_ins, tx_outs, testnet, segwit, sender_account, utxos,receivingAddress_w_amount_list)
+        if self.sign_tx( tx_ins, transaction, sender_account, segwit):
+            return Transaction(transaction, sender_account, tx_ins,utxos ,tx_outs, fee, change, testnet, segwit)
+        else:
+            raise Exception("Signature faliled") 
+            
+            
+class MultiSigTransaction(Transaction):
+    
+    @classmethod
+    def unsigned_tx(self,tx_ins, tx_outs, testnet, segwit, sender_account, utxos,receivingAddress_w_amount_list):
+        
+        my_tx = Tx(1, tx_ins, tx_outs, 0, testnet=testnet, segwit=segwit)
+        
+        fee = self.calculate_fee(1, tx_ins, tx_outs, 0, privkey=sender_account.privkey,
+                                redeem_script=sender_account.redeem_script,testnet=testnet, 
+                                segwit = segwit, multisig =True)
+        
+        change = self.calculate_change(utxos, fee, [x[1] for x in receivingAddress_w_amount_list])
+        
+        my_tx.tx_outs[-1].amount = change
+        return fee, change, my_tx
+
+    @classmethod
+    def sign1by1(self, transaction, sender_account):
+       
+        for tx_input in range(len(transaction.tx_ins)):
+            transaction.sign_input_multisig_1by1(tx_input, sender_account.privkey,sender_account.privkey_index,
+                                                 sender_account.redeem_script, sender_account.n)
+        return transaction
+        
+    @classmethod
+    def verify_signatures(self, transaction, sender_account):
+        if transaction.verify_signatures( sender_account.m):
+            return transaction
+        else:
+            return False
+    
+    @classmethod
+    def create(self, utxo_tx_id_list, receivingAddress_w_amount_list, sender_account, fee=None, #Modify code to allow manual fee!!!
+                 segwit=False):
+        """
+        utxo_tx_id_list: the list of the transaction ids where the UTXOs are.
+        receivingAddress_w_amount_list: a list of tuples (to_address,amount) specifying
+        the amount to send to each address.
+        sender_account: must be an Account object.
+        If fee is specifyed, then the custom fee will be applied.
+        """
+        testnet = self.validate_data(sender_account.address,receivingAddress_w_amount_list)
+        tx_outs = self.get_outputs(receivingAddress_w_amount_list, sender_account)
+        tx_ins_utxo = self.get_inputs(utxo_tx_id_list, sender_account.address, testnet)
+        tx_ins = [x["tx_in"] for x in tx_ins_utxo]
+        utxos = [x["utxo"] for x in tx_ins_utxo]
+        fee, change, transaction = self.unsigned_tx(tx_ins, tx_outs, testnet, segwit, sender_account, utxos,receivingAddress_w_amount_list)
+        transaction = self.sign1by1(transaction, sender_account)
+        final_tx =  self.verify_signatures(transaction, sender_account)
+        if isinstance(final_tx,bool):
+            return MultiSigTransaction(transaction, sender_account, tx_ins,utxos ,tx_outs, fee, change, testnet, segwit)
+        else:
+            return final_tx
+        
+    @classmethod
+    def sign_tx():
+        raise Exception ("Unsupported for multisignature transactions. Use sign1by1() method instead." )
+    
---
+++ bitcoin/basics/final/accounts.py
@@ -0,0 +1,165 @@
+"""
+This code makes use of the code written by Jimmy Song in the book Programming Bitcoin.
+This code only tries to build on Jimmi Song's code and take it to the next step as 
+suggested by him in chapter 14 of his book.
+This module is coded entirely by Oscar Serna.
+
+This is just an educational-purpose code. The author does not take any responsibility
+on any losses caused by the use of this code.
+"""
+
+from ecc import PrivateKey, S256Point, Signature
+from script import p2pkh_script, p2sh_script, Script, p2wpkh_script,p2wsh_script
+from helper import (
+    decode_base58, SIGHASH_ALL, h160_to_p2pkh_address, hash160, 
+    h160_to_p2sh_address, encode_varint)
+from tx import TxIn, TxOut, Tx, TxFetcher
+import hashlib
+from bip32 import Xtended_privkey, Xtended_pubkey
+from bip39 import Mnemonic
+import segwit_addr
+
+class MasterAccount(Xtended_privkey):
+    
+    @classmethod
+    def generate_random(self,entropy = 128,  passphrase="", testnet = False):
+        mnemonic = Mnemonic.generate_random(entropy, passphrase)
+        print(f"Copy these words for future recovery:\n{mnemonic.words}")
+        return self.from_bip39_seed(int.from_bytes(mnemonic.seed,"big"), testnet = testnet)
+        
+    @classmethod
+    def recover_from_words(self,mnemonic_list = None, entropy = 128,  passphrase="", testnet = False):
+        if isinstance(mnemonic_list,str):
+            mnemonic_list = mnemonic_list.split()
+        mnemonic = Mnemonic.recover_from_words(mnemonic_list, entropy ,  passphrase)
+        return self.from_bip39_seed(int.from_bytes(mnemonic.seed,"big"), testnet = testnet)
+    
+        
+
+class Account():
+
+    
+    def __init__(self, _privkey,addr_type = "P2PKH",testnet=False):
+        """
+        Initialize the account with a private key in Integer form.
+        addr_type = String. Possible values: "P2PKH","P2WPKH","P2SH_P2WPKH"
+        testnet: Boolean. If Testnet network is desired, simply specify this value True.
+        Otherwise, simply ommit it.
+        
+        """
+        self.privkey = PrivateKey(_privkey)
+        self.addr_type = addr_type.lower()
+        self.testnet = testnet
+        self.redeem_script_segwit = p2wpkh_script(self.privkey.point.hash160())
+        serialized_redeem = self.redeem_script_segwit.raw_serialize()
+        
+        if self.addr_type == "p2pkh":
+            self.address = self.privkey.point.address(testnet = testnet)
+        elif self.addr_type == "p2wpkh":
+            if self.testnet:
+                self.address = segwit_addr.encode("tb",0,self.privkey.point.hash160())
+            else:
+                self.address = segwit_addr.encode("bc",0,self.privkey.point.hash160())
+        elif self.addr_type == "p2sh_p2wpkh":
+            self.address = h160_to_p2sh_address(hash160(serialized_redeem), testnet=testnet)
+        else:
+            raise Exception("Addres type not supported. Must be p2pkh, p2wpkh, or p2sh_p2wpkh")
+        
+        
+        
+    def __repr__(self):
+        return f"Private Key Hex: {self.privkey.hex()}"
+        
+    
+    @classmethod
+    def from_phrase(cls,phrase, endian="big",addr_type = "P2PKH",testnet=False):
+        """
+        phrase must be in bytes.
+        endian can be either "big" or "little". Change later to boolean "bignendian"
+        Returns an account using the phrase chosen which is converted to an Integer
+        """
+        if endian not in ["big","little"]:
+            raise Exception( f'endian can be either "big" or "little" not "{endian}"')
+            
+        if isinstance(phrase, str):
+            phrase = phrase.encode('utf-8')
+            
+        if isinstance(phrase, bytes):
+            return cls(int.from_bytes(phrase,endian),addr_type,testnet)
+        
+        else:
+            raise Exception( f"The phrase must be a string or bytes, not {type(phrase)}" )
+
+
+class MultSigAccount():
+    
+    def __init__(self,m, n, _privkey, public_key_list, addr_type="p2sh", testnet = False, segwit=True):
+        """
+        Initialize the account with a private key in Integer form.
+        m: the minumin amount of signatures required to spend the money.
+        n: total amount of signatures that can be used to sign transactions.
+        Note: Therefore:
+        a- m has to be less or equal to n.
+        b- n has to be equal to the length of the array _privkeys
+        Also:
+        c- n could be 20 or less, but to keep simplicity in the code, n can only be 16 or less.
+        private_key must be an int.
+        addr_type = String. Possible values: "P2SH","P2WSH","P2SH_P2WSH". NOT case sensitive.
+        Public keys must be in bytes
+        """
+        if n != len(public_key_list):
+            raise Exception("n must be equal to the amount of public keys")
+        if m < 1 or m > 16 or n < 1 or n > 16:
+            raise Exception("m and n must be between 1 and 16")
+        if m > n:
+            raise Exception("m must be always less or equal than n")
+            
+        index = -1
+        pubkey = PrivateKey(_privkey).point.sec()
+        for i,public_key in enumerate(public_key_list):
+            if public_key == pubkey:
+                index = i
+        if index < 0: raise Exception ("Private key must correspond to one of the public keys.")
+    
+        
+        self.privkey = PrivateKey(_privkey)
+        self.public_keys = public_key_list
+        self.m = m
+        self.n = n
+        self.testnet = testnet
+        self.address = h160_to_p2sh_address(hash160(serialized_redeem), testnet=self.testnet)
+        self.addr_type = addr_type.lower()
+        self.privkey_index = index
+        self.redeem_script = Script([m+80, *self.public_keys, n + 80, 174])
+        serialized_redeem = self.redeem_script.raw_serialize()
+      
+        if self.addr_type == "p2sh":
+            self.address = h160_to_p2sh_address(hash160(serialized_redeem), testnet=self.testnet)
+        elif self.addr_type == "p2wsh":
+            if self.testnet:
+                self.address = segwit_addr.encode("tb",0,hashlib.sha256(serialized_redeem).digest())
+            else:
+                self.address = segwit_addr.encode("bc",0,hashlib.sha256(serialized_redeem).digest())
+        elif self.addr_type == "p2sh_p2wsh":
+            address_script = p2wsh_script(hashlib.sha256(serialized_redeem).digest())
+            serialized_addr_script = address_script.raw_serialize()
+            self.address = h160_to_p2sh_address(hash160(serialized_addr_script), testnet=testnet)
+        else:
+            raise Exception("Addres type not supported. Must be p2sh, p2wsh, or p2sh_p2wsh")
+        
+        
+    def __repr__(self):
+        return f"Private Key Hex: {self.privkey.hex()}"
+        
+    
+    @classmethod
+    def from_phrases(cls, m , n , phrases, testnet = False):
+        """
+        phrase: must be a list of tuples of (bytes, endian) or (string, endian). i.e:
+            [(b"my secret","little"),("my other secret","big")]
+            
+        endian: can be either "big" or "little"
+        Returns a multisignature account using the phrases chosen which are converted to Integers
+        """
+        keys = [int.from_bytes(key[0],key[1]) for key in phrases]
+        return cls(m,n,keys,testnet)
